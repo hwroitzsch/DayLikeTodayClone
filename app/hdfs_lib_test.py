dd7c12d7f402a25c55f0076e9868bea36e2a0c75
Your Name <you@example.com>
2016-01-12 18:19:54 +0100

    first try python-dfs

--- app/hdfs_lib_test.py
+++ app/hdfs_lib_test.py
@@ -40,5 +40,13 @@ from hdfs import InsecureClient
 # 		pass
 
 # pig -param input=<input file> -param output=<output file> -param reducer-num=1 -param date=1980-12-24
+try:
+    client = Config().get_client('dev')
 
-client = Config().get_client('dev')
+
+    with client.read('Serien_de',encoding='utf-8',delimiter='\n') as reader:
+        for line in reader:
+            print(line)
+except:
+    print("schief gelaufen beim Lesen aus HDFS")
+    raise
