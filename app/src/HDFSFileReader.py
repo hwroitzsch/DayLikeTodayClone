4516453ed03ba5a326fef90595157dbec934cdd9
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-01-13 11:45:21 +0100

    Parsers added

---
+++ app/src/HDFSFileReader.py
@@ -0,0 +1,25 @@
+__author__ = 'Hans-Werner Roitzsch'
+
+from hdfs import Config
+
+class HDFSFileReader:
+	"""This class represents a reader for accessing files in a HDFS."""
+
+	def __init__(self):
+		pass
+
+	@classmethod
+	def read(cls, file_path):
+		lines = []
+
+		try:
+			client = Config().get_client('dev')
+
+			with client.read('Serien_de', encoding='utf-8', delimiter='\n') as reader:
+				for line in reader:
+					lines.append(line)  # eventuell unnoetig, kann man auch reader zurueckgeben?
+		except:
+			print("ERROR: Could not read from HDFS.")
+			raise
+
+		return lines
